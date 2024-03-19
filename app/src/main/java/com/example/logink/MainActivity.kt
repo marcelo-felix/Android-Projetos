4bd27c4bafd55c3cc6a096e654696d81d1d256c7
doutor <doutor793@gmail.com>
2024-03-19 15:16:19 -0300

    start

--- app/src/main/java/com/example/logink/MainActivity.kt
+++ app/src/main/java/com/example/logink/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.logink
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.logink.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var busca:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        busca = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(busca.root)
+
+        busca.btnEnt.setOnClickListener {
+            if (busca.inpEmail.text.toString() == "" && busca.inpPasswd.text.toString() == "") run {
+                Toast.makeText(this, "Penchant Todos os Campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
